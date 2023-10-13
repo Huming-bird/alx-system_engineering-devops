2171d3348d978bfcce7933f5aa2dd0cba26258f7
Huming-bird <ahmedolalekan2@gmail.com>
2023-10-13 08:19:59 -0700

    debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# webstack debugging
+
+exec {'debug':
+  command   => '/bin/bash -c "sed -i s/phpp/php/g /var/www/html/wp-settings.php && service apache2 restart"',
+  provider  => 'shell',
+  logoutput => true,
+}
